673d0b6b2a93cb367e8d226cb3e3fc3800963649
Claudia Beresford <claudia@ori.co>
2020-04-11 17:27:42 +0100

    part 6

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,13 +1,5 @@
 package fizzbuzz
 
-func IsDivisibleByThree(num int) bool {
-	return num%3 == 0
-}
-
-func IsDivisibleByFive(num int) bool {
-	return num%5 == 0
-}
-
-func IsDivisibleByThreeAndFive(num int) bool {
-	return num%15 == 0
+func IsDivisibleBy(number, divisor int) bool {
+	return number%divisor == 0
 }
