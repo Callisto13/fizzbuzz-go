487ac61ebe82c04beff80bc193a2cdc64542e832
Claudia Beresford <claudia@ori.co>
2020-04-11 17:33:28 +0100

    part 7

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,5 +1,20 @@
 package fizzbuzz
 
+import "fmt"
+
 func IsDivisibleBy(number, divisor int) bool {
 	return number%divisor == 0
+}
+
+func Says(number int) string {
+	switch {
+	case IsDivisibleBy(number, 15):
+		return "fizzbuzz"
+	case IsDivisibleBy(number, 3):
+		return "fizz"
+	case IsDivisibleBy(number, 5):
+		return "buzz"
+	default:
+		return fmt.Sprintf("%d", number)
+	}
 }
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -33,4 +33,22 @@ var _ = Describe("Fizzbuzz", func() {
 			Expect(fizzbuzz.IsDivisibleBy(1, 15)).To(BeFalse())
 		})
 	})
+
+	Context("when playing the game, Fizzbuzz says...", func() {
+		It("`fizz` when a number is divisible by 3", func() {
+			Expect(fizzbuzz.Says(3)).To(Equal("fizz"))
+		})
+
+		It("`buzz` when a number is divisible by 5", func() {
+			Expect(fizzbuzz.Says(5)).To(Equal("buzz"))
+		})
+
+		It("`buzz` when a number is divisible by 3 and 5", func() {
+			Expect(fizzbuzz.Says(15)).To(Equal("fizzbuzz"))
+		})
+
+		It("the number when it is NOT divisible by 3 or 5", func() {
+			Expect(fizzbuzz.Says(7)).To(Equal("7"))
+		})
+	})
 })
