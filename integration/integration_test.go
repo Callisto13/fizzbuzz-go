959b669009262311421a5e95945ba08ee635a3f9
Claudia Beresford <claudia@ori.co>
2020-04-11 17:56:33 +0100

    part 12

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -33,12 +33,13 @@ var _ = Describe("Integration", func() {
 		Eventually(session.Out).Should(gbytes.Say("fizz\n"))
 	})
 
-	It("when the command line argument is 5, it prints 'buzz'", func() {
-		fizzbuzzCommand = exec.Command(fizzbuzzBinary, "5")
+	It("when there are several command line argument, it processes them all", func() {
+		args := []string{"1", "2", "3", "4", "5"}
+		fizzbuzzCommand = exec.Command(fizzbuzzBinary, args...)
 
 		session, err := gexec.Start(fizzbuzzCommand, GinkgoWriter, GinkgoWriter)
 		Expect(err).NotTo(HaveOccurred())
-		Eventually(session.Out).Should(gbytes.Say("buzz\n"))
+		Eventually(session.Out).Should(gbytes.Say("1\n2\nfizz\n4\nbuzz\n"))
 	})
 
 	It("when the command line argument is not a number, it prints an error", func() {
@@ -49,12 +50,11 @@ var _ = Describe("Integration", func() {
 		Eventually(session.Out).Should(gbytes.Say("input is not a number, skipping\n"))
 	})
 
-	It("when there are several command line argument, it processes them all", func() {
-		args := []string{"1", "2", "3", "4", "5"}
-		fizzbuzzCommand = exec.Command(fizzbuzzBinary, args...)
+	It("when no arguments are provided, it prints usage info", func() {
+		fizzbuzzCommand = exec.Command(fizzbuzzBinary)
 
 		session, err := gexec.Start(fizzbuzzCommand, GinkgoWriter, GinkgoWriter)
 		Expect(err).NotTo(HaveOccurred())
-		Eventually(session.Out).Should(gbytes.Say("1\n2\nfizz\n4\nbuzz\n"))
+		Eventually(session.Out).Should(gbytes.Say("usage: fizzbuzz <number> \\[<number> <number> ...\\]\n"))
 	})
 })
--- main.go
+++ main.go
@@ -11,6 +11,11 @@ import (
 func main() {
 	numbersAsStrings := os.Args[1:]
 
+	if len(numbersAsStrings) == 0 {
+		fmt.Println("usage: fizzbuzz <number> [<number> <number> ...]")
+		os.Exit(1)
+	}
+
 	for _, value := range numbersAsStrings {
 		number, err := strconv.Atoi(value)
 		if err != nil {
